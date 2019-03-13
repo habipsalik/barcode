1ab4c640531fec4ea5b0a41a3b4c7fee531c4698
habipsalik <habip.salik@metglobal.com>
2019-03-13 14:26:00 +0300

    Fix setup file and tkinter settings

--- main.py
+++ main.py
@@ -1,12 +1,28 @@
 #!/usr/bin/env python
+import sys
 try:
-    import tkinter as tk
+    from Tkinter import *
 except ImportError:
-    import Tkinter as tk
+    try:
+        from Tkinter import Tk
+    except:
+        # If no versions of tkinter exist (most likely linux) provide a message
+        if sys.version_info.major < 3:
+            print("Error: Tkinter not found")
+            print('For linux, you can install Tkinter by executing: "sudo apt-get install python-tk"')
+            sys.exit(1)
+        else:
+            print("Error: tkinter not found")
+            print('For linux, you can install tkinter by executing: "sudo apt-get install python3-tk"')
+            sys.exit(1)
+try:
+    from tkinter.filedialog import askopenfilename, askdirectory, askopenfilenames
+except ImportError:
+    from tkFileDialog import askopenfilename, askdirectory, askopenfilenames
+
 from pyzbar import pyzbar
 import cv2
-from tkinter.filedialog import askopenfilename
-import sys
+import os
 
 
 class Window:
@@ -47,11 +63,10 @@ class Window:
 
         # writer file path in text label
         file_path = self.browse_file()
+        self.text_label['text'] = file_path
 
         list_of_codes = ["30P", "1T", "Q", "1P", "9D", "P", "K", "21L", "S", "4L", "10D"]
         zbar_output = self.zbar(file_path)
-
-        print(zbar_output)
 
         fine_result = {}
 
@@ -66,7 +81,6 @@ class Window:
                     pass
                     # print('{} = Not Found!'.format(code.lower()))
 
-        self.text_label['text'] = file_path
         self.results1['text'] = fine_result['1p']
         self.results2['text'] = fine_result['p']
         self.results3['text'] = fine_result['q']
@@ -74,7 +88,7 @@ class Window:
         self.results5['text'] = fine_result['10d']
 
     def browse_file(self):
-        tk.Tk().withdraw()
+        Tk().withdraw()
         file_path = askopenfilename()
         return file_path
 
@@ -87,94 +101,94 @@ class Window:
         boss.maxsize(width=700, height=600)
         bg_color = '#485885'
 
-        C = tk.Canvas(boss)
-        self.background_image = tk.PhotoImage(file='./t2.png')
-        background_label = tk.Label(boss, image=self.background_image)
+        C = Canvas(boss)
+        #self.background_image = PhotoImage(file='./t2.png')
+        background_label = Label(boss, bg=bg_color)
         background_label.place(x=0, y=0, relwidth=1, relheight=1)
 
         C.pack()
 
-        frame = tk.LabelFrame(boss, bg=bg_color, bd=10)
+        frame = LabelFrame(boss, bg=bg_color, bd=10)
         frame.place(relx=0.5, rely=0.07, relwidth=0.75, relheight=0.2, anchor='n')
 
-        # frame1 = tk.Frame(boss, bg='#3d4142', bd=5)
+        # frame1 = Frame(boss, bg='#3d4142', bd=5)
         # frame1.place(relx=0.5, rely=0.25, relwidth=0.75, relheight=0.1, anchor='n')
         # frame_window = C.create_window(100, 40, window=frame)
 
-        # self.textbox = tk.Entry(self.frame, font=40, bg='#b8c3c8')
+        # self.textbox = Entry(self.frame, font=40, bg='#b8c3c8')
         # self.textbox.place(relwidth=0.65, relheight=1)
 
-        self.text_label = tk.Label(frame, anchor='sw', justify='left', bd=10)
+        self.text_label = Label(frame, anchor='sw', justify='left', bd=10)
         self.text_label.config(font=40, bg='#ffffff', fg=bg_color)
         self.text_label.place(relx=0.25, rely=0.47, relwidth=0.74, relheight=0.4)
 
-        path_text = tk.Label(frame, text="File Path: ", anchor='sw', justify='left', bg=bg_color,
+        path_text = Label(frame, text="File Path: ", anchor='sw', justify='left', bg=bg_color,
                              fg="white")
         path_text.place(relx=0.04, rely=0.47, relwidth=0.19, relheight=0.4)
 
-        submit = tk.Button(frame, text='Browse', font=40, fg=bg_color, command=lambda: self.parser())
+        submit = Button(frame, text='Browse', font=40, fg=bg_color, command=lambda: self.parser())
         submit.place(relx=0.02, rely=0.06, relwidth=0.3, relheight=0.3)
-        clear = tk.Button(frame, text="Clear", font=40, fg=bg_color, command=lambda: [
+        clear = Button(frame, text="Clear", font=40, fg=bg_color, bg=bg_color, command=lambda: [
             self.clear_widget_text(self.text_label), self.clear_widget_text(self.results1),
             self.clear_widget_text(self.results2), self.clear_widget_text(self.results3),
             self.clear_widget_text(self.results4), self.clear_widget_text(self.results5)])
 
         clear.place(relx=0.355, rely=0.06, relwidth=0.3, relheight=0.3)
-        quit = tk.Button(frame, text="Quit", font=40, fg=bg_color, command=lambda: sys.exit(1))
+        quit = Button(frame, text="Quit", font=40, fg=bg_color, command=lambda: sys.exit(1))
         quit.place(relx=0.69, rely=0.06, relwidth=0.3, relheight=0.3)
 
-        # self.bbutton = tk.Button(self.frame1, text="Read", command=lambda: self.zbar())
+        # self.bbutton = Button(self.frame1, text="Read", command=lambda: self.zbar())
         # self.bbutton.place(relx=0.7, relheight=1, relwidth=0.3)
 
-        # lower_frame = tk.Frame(boss, bg='#3d4142', bd=10)
+        # lower_frame = Frame(boss, bg='#3d4142', bd=10)
         # lower_frame.place(relx=0.5, rely=0.25, relwidth=0.75, relheight=0.5, anchor='')
 
-        lower_frame = tk.LabelFrame(boss, fg="white", font=14, text="  Results  ", bg=bg_color, bd=15)
+        lower_frame = LabelFrame(boss, fg="white", font=14, text="  Results  ", bg=bg_color, bd=15)
         lower_frame.pack(fill="both", expand="yes")
         lower_frame.place(relx=0.5, rely=0.32, relwidth=0.75, relheight=0.6, anchor='n')
 
-        results1_text = tk.Label(lower_frame, text="Part No: ", font=40, anchor='sw', justify='left', bg=bg_color,
+        results1_text = Label(lower_frame, text="Part No: ", font=40, anchor='sw', justify='left', bg=bg_color,
                                  fg="white")
         results1_text.place(relx=0.04, rely=0.04, relwidth=0.4, relheight=0.15)
 
-        results2_text = tk.Label(lower_frame, text="Customer Part No: ", font=40, anchor='sw', justify='left',
+        results2_text = Label(lower_frame, text="Customer Part No: ", font=40, anchor='sw', justify='left',
                                  bg=bg_color, fg="white")
         results2_text.place(relx=0.04, rely=0.23, relwidth=0.4, relheight=0.15)
 
-        results3_text = tk.Label(lower_frame, text="Quantity: ", font=40, anchor='sw', justify='left', bg=bg_color,
+        results3_text = Label(lower_frame, text="Quantity: ", font=40, anchor='sw', justify='left', bg=bg_color,
                                  fg="white")
         results3_text.place(relx=0.04, rely=0.42, relwidth=0.4, relheight=0.15)
 
-        results4_text = tk.Label(lower_frame, text="Lot No: ", font=40, anchor='sw', justify='left', bg=bg_color,
+        results4_text = Label(lower_frame, text="Lot No: ", font=40, anchor='sw', justify='left', bg=bg_color,
                                  fg="white")
         results4_text.place(relx=0.04, rely=0.61, relwidth=0.4, relheight=0.15)
 
-        results5_text = tk.Label(lower_frame, text="Date: ", font=40, anchor='sw', justify='left', bg=bg_color,
+        results5_text = Label(lower_frame, text="Date: ", font=40, anchor='sw', justify='left', bg=bg_color,
                                  fg="white")
         results5_text.place(relx=0.04, rely=0.80, relwidth=0.4, relheight=0.15)
 
-        self.results1 = tk.Label(lower_frame, anchor='sw', font=40, justify='left', fg=bg_color, bd=10)
+        self.results1 = Label(lower_frame, anchor='sw', font=40, justify='left', fg=bg_color, bd=10)
         # self.results.config(font=40, bg=bg_color)
         self.results1.place(relx=0.49, rely=0.04, relwidth=0.5, relheight=0.15)
 
-        self.results2 = tk.Label(lower_frame, anchor='sw', font=40, justify='left', fg=bg_color, bd=10)
+        self.results2 = Label(lower_frame, anchor='sw', font=40, justify='left', fg=bg_color, bd=10)
         # self.results.config(font=40, bg=bg_color)
         self.results2.place(relx=0.49, rely=0.23, relwidth=0.5, relheight=0.15)
 
-        self.results3 = tk.Label(lower_frame, anchor='sw', font=40, justify='left', fg=bg_color, bd=10)
+        self.results3 = Label(lower_frame, anchor='sw', font=40, justify='left', fg=bg_color, bd=10)
         # self.results.config(font=40, bg=bg_color)
         self.results3.place(relx=0.49, rely=0.42, relwidth=0.5, relheight=0.15)
 
-        self.results4 = tk.Label(lower_frame, anchor='sw', font=40, justify='left', fg=bg_color, bd=10)
+        self.results4 = Label(lower_frame, anchor='sw', font=40, justify='left', fg=bg_color, bd=10)
         # self.results.config(font=40, bg=bg_color)
         self.results4.place(relx=0.49, rely=0.61, relwidth=0.5, relheight=0.15)
 
-        self.results5 = tk.Label(lower_frame, anchor='sw', font=40, justify='left', fg=bg_color, bd=10)
+        self.results5 = Label(lower_frame, anchor='sw', font=40, justify='left', fg=bg_color, bd=10)
         # self.results.config(font=40, bg=bg_color)
         self.results5.place(relx=0.49, rely=0.80, relwidth=0.5, relheight=0.15)
 
 
-boss = tk.Tk()
+boss = Tk()
 boss.title("B-OSS")
 window = Window(boss)
 boss.protocol("WM_DELETE_WINDOW", lambda: sys.exit(1))
--- setup.py
+++ setup.py
@@ -1,4 +1,17 @@
-from distutils.core import setup
-import py2exe
+import cx_Freeze
+import sys
 
-setup(console=['main.py'])
+base = None
+
+if sys.platform == 'win32':
+    base = "Win32GUI"
+
+executables = [cx_Freeze.Executable("main.py", base=base, icon="b-oss1.ico")]
+
+cx_Freeze.setup(
+    name = "B-OSS",
+    options = {"build_exe": {"packages":["Tkinter", "pyzbar", "numpy"], "include_files":["b-oss1.ico"]}},
+    version = "0.01",
+    description = "Barcodes application",
+    executables = executables
+    )
